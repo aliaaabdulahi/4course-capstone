7d7b59cb9cbe2d096795d7c5125ceab9775eb030
Shiyang Chen <ana.chensy@gmail.com>
2021-05-17 17:31:06 -0400

    change the table name

--- server/db/models/user-event.js
+++ server/db/models/user-event.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 
-const User_Event = db.define("user-event", {
+const User_Event = db.define("User_Events", {
   invitees: Sequelize.ARRAY(Sequelize.INTEGER),
 });
 
