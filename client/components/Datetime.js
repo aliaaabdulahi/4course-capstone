21cb9a3f84bad6a95f0c6201783fd048c1d1498b
Annie Moosher <annie.sophia219@gmail.com>
2021-05-17 22:14:48 -0400

    refactored home and datetime component

--- client/components/Datetime.js
+++ client/components/Datetime.js
@@ -1,45 +1,106 @@
-import React, {useState} from 'react';
-import DateTimePicker from 'react-datetime-picker';
-import {createNewEventThunk} from '../store/events';
-import {connect} from 'react-redux'
+import React, { useState } from "react";
+import DateTimePicker from "react-datetime-picker";
+import { createNewEventThunk } from "../store/events";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { connect } from "react-redux";
 
-function Datetime ({handleSave, history}) {
-  const [value, onChange] = useState (new Date ());
+const useStyles = makeStyles((theme) => ({
+  dateTimeButton: {
+    ...theme.typography.button,
+    fontFamily: "Permanent Marker",
+    color: "#fff100",
+    marginTop: "1em",
+    "&:hover": {
+      color: "#DC143C",
+      backgroundColor: "fff100",
+    },
+  },
+  text: {
+    color: "#fff100",
+    fontFamily: "Permanent Marker",
+    backgroundColor: "#DC143C",
+    borderRadius: 10,
+  },
+  picker: {
+    color: "#fff100",
+  },
+  container: {
+    backgroundColor: "#fff100",
+  },
+}));
+
+function Datetime({ handleSave, history }) {
+  const classes = useStyles();
+  const [value, onChange] = useState(new Date());
 
   const onSave = (e) => {
     console.log("saved datetime", formatDate(value));
     handleSave(e, formatDate(value));
     history.push(`/startchallenge`);
-  }
+  };
 
   const formatDate = (date) => {
     var YYYY = date.getFullYear().toString(),
-        MM = (date.getMonth()+1).toString(),
-        DD  = date.getDate().toString(),
-        hh = date.getUTCHours().toString(),
-        mm = date.getUTCMinutes().toString(),
-        ss = date.getUTCSeconds().toString();
-    return YYYY + "-" + (MM[1]?MM:"0"+MM[0]) + "-" + (DD[1]?DD:"0"+DD[0]) + " " + (hh[1]?hh:"0"+hh[0]) + ":" + (mm[1]?mm:"0"+mm[0]) + ":" + (ss[1]?ss:"0"+ss[0]);
-};
+      MM = (date.getMonth() + 1).toString(),
+      DD = date.getDate().toString(),
+      hh = date.getUTCHours().toString(),
+      mm = date.getUTCMinutes().toString(),
+      ss = date.getUTCSeconds().toString();
+    return (
+      YYYY +
+      "-" +
+      (MM[1] ? MM : "0" + MM[0]) +
+      "-" +
+      (DD[1] ? DD : "0" + DD[0]) +
+      " " +
+      (hh[1] ? hh : "0" + hh[0]) +
+      ":" +
+      (mm[1] ? mm : "0" + mm[0]) +
+      ":" +
+      (ss[1] ? ss : "0" + ss[0])
+    );
+  };
 
   return (
-    <div className="center">
-      <h1>Choose Date & Time</h1>
-      <div className="center">
-        <DateTimePicker onChange={onChange} value={value} />
-      </div>
-      <button className="button bigCircle" onClick={onSave}>Save</button>
-    </div>
+    <Grid container direction="column" className={classes.container}>
+      <Grid container justify="center" alignItems="stretch" direction="row">
+        <Grid item>
+          <h1 className={classes.text}>Choose Date & Time</h1>
+          <div className="center">
+            <DateTimePicker
+              className={classes.picker}
+              onChange={onChange}
+              value={value}
+            />
+          </div>
+          <Grid container item direction="row" justify="center">
+            <Grid item>
+              <Button
+                variant="contained"
+                className={classes.dateTimeButton}
+                onClick={onSave}
+                color="secondary"
+                size="large"
+              >
+                Save
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSave(evt, date) {
-      evt.preventDefault()
-      dispatch(createNewEventThunk(date))
-    }
-  }
-}
+      evt.preventDefault();
+      dispatch(createNewEventThunk(date));
+    },
+  };
+};
 
 export default connect(null, mapDispatch)(Datetime);
--- client/components/RestaurantContainer.js
+++ client/components/RestaurantContainer.js
@@ -21,10 +21,10 @@ const useStyles = makeStyles((theme) => ({
 const RestaurantContainer = (props) => {
   const classes = useStyles();
   return (
-    <Grid container direction="row" justify="flex-end" alignItems="center">
+    <Grid container direction="row" justify="flex-end">
       <ul>
         {props.resSelections.map((res) => (
-          <Grid container item key={res.yelpId}>
+          <Grid item key={res.yelpId}>
             <li className={classes.listItem}>{res.yelpName}</li>
             <Button
               className={classes.resListButton}
