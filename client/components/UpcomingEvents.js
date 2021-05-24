d0ed17e39655798247ebe84483d3635773c5a0db
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-05-24 12:47:57 -0400

    revert upcoming events back to old version

--- client/components/UpcomingEvents.js
+++ client/components/UpcomingEvents.js
@@ -1,62 +1,76 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {upcomingEventsThunk} from '../store/events';
-import SingleEvent from './SingleEvent'
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { upcomingEventsThunk } from "../store/events";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import SingleEvent from "./SingleEvent";
+import SingleEvent from "./SingleEvent";
 
 class UpcomingEvents extends React.Component {
-  constructor (props) {
-    super (props);
-  }
-
-   componentDidMount () {
-     this.props.upcomingEventsThunk (this.props.id);
+  constructor(props) {
+    super(props);
   }
 
   formatDate(timestamp) {
     const date = new Date(timestamp);
-    return date.getMonth()+1 + "-" + date.getDate() + "-" + date.getFullYear();
+    return (
+      date.getMonth() + 1 + "-" + date.getDate() + "-" + date.getFullYear()
+    );
   }
-
   formatTime(timestamp) {
     const date = new Date(timestamp);
-    date.setHours( date.getHours());
-    return ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
+    date.setHours(date.getHours());
+    return (
+      ("0" + date.getHours()).slice(-2) +
+      ":" +
+      ("0" + date.getMinutes()).slice(-2) +
+      ":" +
+      ("0" + date.getSeconds()).slice(-2)
+    );
   }
 
-  render () {
-    console.log(this.props.events)
+  render() {
     return (
       <div className="center shape">
-        <h2 className="font">
-          Upcoming Events
-        </h2>
-        <div className="flex-center">
-          {this.props.events !== undefined && this.props.events.length > 0
-            ? this.props.events.map (event => (
-              <div>
-                <Link to={`/events/${event.id}`}>
-                <div className="event">
-                  <p>{event.name}</p>
-                  <img style={{width: '100px'}} src={JSON.parse(event.restaurants[0]).yelpImageUrl}></img>
-                  <p>{this.formatDate(event.date)}</p>
-                  <p>{this.formatTime(event.date)}</p>
-                </div>
-                </Link>
-                </div>
-              ))
-            : null}
+        <div style={{ padding: 20 }}>
+          <h2 className="font">Upcoming Events</h2>
+          <Grid container justify="center">
+            {this.props.events !== undefined && this.props.events.length > 0
+              ? this.props.events.map((event) => (
+                  <Grid item md style={{ margin: 10 }}>
+                    <Paper
+                      style={{ padding: 5, maxWidth: 500, maxHeight: 500 }}
+                    >
+                      <Link to={`/events/${event.id}`}>
+                        <Typography>{event.name}</Typography>
+                        <img
+                          style={{ width: "300px" }}
+                          src={JSON.parse(event.restaurants[0]).yelpImageUrl}
+                        ></img>
+                        <Typography>
+                          Date: {this.formatDate(event.date)}
+                        </Typography>
+                        <Typography>
+                          Time: {this.formatTime(event.date)}
+                        </Typography>
+                      </Link>
+                    </Paper>
+                  </Grid>
+                ))
+              : null}
+          </Grid>
         </div>
       </div>
     );
   }
 }
-
-const mapState = state => {
+const mapState = (state) => {
   return {
     id: state.auth.id,
     events: state.events.events,
   };
 };
 
-export default connect (mapState, {upcomingEventsThunk}) (UpcomingEvents);
+export default connect(mapState, { upcomingEventsThunk })(UpcomingEvents);
